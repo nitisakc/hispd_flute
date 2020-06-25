d16975b7e0561edb92ce9caff86090bb127bc721
Nitisak Chancham <nc.north@gmail.com>
2020-06-25 10:49:16 +0700

    init

---
+++ hispd_flute.ino
@@ -0,0 +1,44 @@
+const int PIN_SPD_A_INPUT = 2;
+const int PIN_SPD_B_INPUT = 3;
+const int PIN_MOTOR_LEFT = 4;
+const int PIN_MOTOR_RIGHT = 5;
+const int PIN_SENSOR_LEFT_INPUT = 18;
+const int PIN_SENSOR_RIGHT_INPUT = 19;
+const int PPR = 400;
+
+long count = 0;
+int spd, b, sensorLeft, sensorRight;
+
+void triggerSpdInput() {
+  spd = digitalRead(PIN_SPD_A_INPUT);
+//  b = digitalRead(PIN_SPD_B_INPUT);
+  sensorLeft = digitalRead(PIN_SENSOR_LEFT_INPUT);
+  sensorRight = digitalRead(PIN_SENSOR_RIGHT_INPUT);
+  
+  digitalWrite(LED_BUILTIN, spd);
+  Serial.println(count++ / (PPR * 2));
+  
+  if(sensorLeft == sensorRight){
+    digitalWrite(PIN_MOTOR_LEFT, spd);
+    digitalWrite(PIN_MOTOR_RIGHT, spd);
+  }else{
+    digitalWrite(PIN_MOTOR_LEFT, sensorLeft == 0 ? sensorLeft : spd);
+    digitalWrite(PIN_MOTOR_RIGHT,  sensorRight == 0 ? sensorRight : spd);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(PIN_MOTOR_LEFT, OUTPUT);
+  pinMode(PIN_MOTOR_RIGHT, OUTPUT);
+  
+  pinMode(PIN_SPD_A_INPUT, INPUT_PULLUP); digitalWrite(PIN_SPD_A_INPUT, HIGH);
+  pinMode(PIN_SPD_B_INPUT, INPUT_PULLUP); digitalWrite(PIN_SPD_B_INPUT, HIGH);
+  pinMode(PIN_SENSOR_LEFT_INPUT, INPUT_PULLUP);
+  pinMode(PIN_SENSOR_RIGHT_INPUT, INPUT_PULLUP);
+  
+  attachInterrupt(digitalPinToInterrupt(PIN_SPD_A_INPUT), triggerSpdInput, CHANGE); //FALLING
+}
+
+void loop() {}
