4ab7648818e56a19877c6de012c17a035f7e36c3
Nitisak Chancham <nc.north@gmail.com>
2020-06-26 15:56:07 +0700

    issue dir spd

--- hispd_flute.ino
+++ hispd_flute.ino
@@ -1,10 +1,11 @@
 const int PIN_SPD_A1_INPUT = 2;         //encoder A
 const int PIN_SPD_A2_INPUT = 3;         //encoder A
-const int PIN_SPD_B_INPUT = 4;          //encoder B
+const int PIN_SPD_B1_INPUT = 18;        //encoder B
+const int PIN_SPD_B2_INPUT = 19;        //encoder B
 const int PIN_MOTOR_LEFT_A = 5;         //Motor Left A
 const int PIN_MOTOR_LEFT_B = 6;         //Motor Left B
 const int PIN_MOTOR_RIGHT_A = 7;        //Motor Right A
-const int PIN_MOTOR_RIGHT_B = 8;        //Motor Right A
+const int PIN_MOTOR_RIGHT_B = 8;        //Motor Right B
 const int PIN_SENSOR_LEFT_INPUT = 9;    //Sensor Left
 const int PIN_SENSOR_RIGHT_INPUT = 10;  //Sensor Right
 const int PPR = 1000;                   //Encoder PPR
@@ -12,56 +13,70 @@ const int PPR = 1000;                   //Encoder PPR
 long count = 0;
 int spd, b, sensorLeft, sensorRight;
 
-void triggerSpdInputFalling() {
-  b = digitalRead(PIN_SPD_B_INPUT);
+void triggerSpdInputFallingA() {
   sensorLeft = digitalRead(PIN_SENSOR_LEFT_INPUT);
   sensorRight = digitalRead(PIN_SENSOR_RIGHT_INPUT);
-  digitalWrite(LED_BUILTIN, spd);
-//  Serial.println("0");
-//  Serial.print("A: 0, B: ");
-//  Serial.println(b);
-  
-  if(sensorLeft == sensorRight){
+
+  if(sensorLeft == sensorRight){ท 
     digitalWrite(PIN_MOTOR_LEFT_A, 0);
-    digitalWrite(PIN_MOTOR_LEFT_B, b);
-    
     digitalWrite(PIN_MOTOR_RIGHT_A, 0);
-    digitalWrite(PIN_MOTOR_RIGHT_B, b);
   }else{
     if(sensorLeft == 1){
       digitalWrite(PIN_MOTOR_LEFT_A, 0);
-      digitalWrite(PIN_MOTOR_LEFT_B, b);
     }
     if(sensorRight == 1){
       digitalWrite(PIN_MOTOR_RIGHT_A, 0);
-      digitalWrite(PIN_MOTOR_RIGHT_B, b);
     }
   }
 }
 
-void triggerSpdInputRising() {
-  b = digitalRead(PIN_SPD_B_INPUT);
+void triggerSpdInputRisingA() {
   sensorLeft = digitalRead(PIN_SENSOR_LEFT_INPUT);
   sensorRight = digitalRead(PIN_SENSOR_RIGHT_INPUT);
-  digitalWrite(LED_BUILTIN, spd);
-//  Serial.println("1");
-//  Serial.print("A: 1, B: ");
-//  Serial.println(b);
   
   if(sensorLeft == sensorRight){
     digitalWrite(PIN_MOTOR_LEFT_A, 1);
-    digitalWrite(PIN_MOTOR_LEFT_B, b);
-    
     digitalWrite(PIN_MOTOR_RIGHT_A, 1);
-    digitalWrite(PIN_MOTOR_RIGHT_B, b);
   }else{
     if(sensorLeft == 1){
       digitalWrite(PIN_MOTOR_LEFT_A, 1);
-      digitalWrite(PIN_MOTOR_LEFT_B, b);
     }
     if(sensorRight == 1){
       digitalWrite(PIN_MOTOR_RIGHT_A, 1);
-      digitalWrite(PIN_MOTOR_RIGHT_B, b);
+    }
+  }
+}
+
+void triggerSpdInputFallingB() {
+  sensorLeft = digitalRead(PIN_SENSOR_LEFT_INPUT);
+  sensorRight = digitalRead(PIN_SENSOR_RIGHT_INPUT);
+
+  if(sensorLeft == sensorRight){
+    digitalWrite(PIN_MOTOR_LEFT_B, 0);
+    digitalWrite(PIN_MOTOR_RIGHT_B, 0);
+  }else{
+    if(sensorLeft == 1){
+      digitalWrite(PIN_MOTOR_LEFT_B, 0);
+    }
+    if(sensorRight == 1){
+      digitalWrite(PIN_MOTOR_RIGHT_B, 0);
+    }
+  }
+}
+
+void triggerSpdInputRisingB() {
+  sensorLeft = digitalRead(PIN_SENSOR_LEFT_INPUT);
+  sensorRight = digitalRead(PIN_SENSOR_RIGHT_INPUT);
+  
+  if(sensorLeft == sensorRight){
+    digitalWrite(PIN_MOTOR_LEFT_B, 1);
+    digitalWrite(PIN_MOTOR_RIGHT_B, 1);
+  }else{
+    if(sensorLeft == 1){
+      digitalWrite(PIN_MOTOR_LEFT_B, 1);
+    }
+    if(sensorRight == 1){
+      digitalWrite(PIN_MOTOR_RIGHT_B, 1);
     }
   }
 }
@@ -76,12 +91,15 @@ void setup() {
   
   pinMode(PIN_SPD_A1_INPUT, INPUT_PULLUP); digitalWrite(PIN_SPD_A1_INPUT, HIGH);
   pinMode(PIN_SPD_A2_INPUT, INPUT_PULLUP); digitalWrite(PIN_SPD_A2_INPUT, HIGH);
-  pinMode(PIN_SPD_B_INPUT, INPUT_PULLUP); digitalWrite(PIN_SPD_B_INPUT, HIGH);
+  pinMode(PIN_SPD_B1_INPUT, INPUT_PULLUP); digitalWrite(PIN_SPD_B1_INPUT, HIGH);
+  pinMode(PIN_SPD_B2_INPUT, INPUT_PULLUP); digitalWrite(PIN_SPD_B2_INPUT, HIGH);
   pinMode(PIN_SENSOR_LEFT_INPUT, INPUT_PULLUP);
   pinMode(PIN_SENSOR_RIGHT_INPUT, INPUT_PULLUP);
   
-  attachInterrupt(digitalPinToInterrupt(PIN_SPD_A1_INPUT), triggerSpdInputFalling, FALLING); 
-  attachInterrupt(digitalPinToInterrupt(PIN_SPD_A2_INPUT), triggerSpdInputRising, RISING); 
+  attachInterrupt(digitalPinToInterrupt(PIN_SPD_A1_INPUT), triggerSpdInputFallingA, FALLING); 
+  attachInterrupt(digitalPinToInterrupt(PIN_SPD_A2_INPUT), triggerSpdInputRisingA, RISING); 
+  attachInterrupt(digitalPinToInterrupt(PIN_SPD_B1_INPUT), triggerSpdInputFallingB, FALLING); 
+  attachInterrupt(digitalPinToInterrupt(PIN_SPD_B2_INPUT), triggerSpdInputRisingB, RISING); 
 }
 
 void loop() {}
