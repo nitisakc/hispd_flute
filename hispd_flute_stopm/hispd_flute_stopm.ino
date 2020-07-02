3879fb7eb1b157633f6d964d7c8d6d9b5a9031dc
Nitisak Chancham <nc.north@gmail.com>
2020-07-02 14:59:05 +0700

    sensor bypass

--- hispd_flute_stopm/hispd_flute_stopm.ino
+++ hispd_flute_stopm/hispd_flute_stopm.ino
@@ -1,5 +1,6 @@
 const int PIN_SENSOR_LEFT_INPUT   = 2;      //Sensor Left
 const int PIN_SENSOR_RIGHT_INPUT  = 3;      //Sensor Right
+const int PIN_SENSOR_BYPASS_INPUT = 4;      //Sensor Bypass
 const int PIN_SENSOR_LEFT_OUTPUT  = 6;      //Servo Control Left
 const int PIN_SENSOR_RIGHT_OUTPUT = 7;      //Servo Control Right
 
@@ -16,13 +17,14 @@ Left   |   Right  |   Left   |   Right
   
  */
 
-int left, right;
+int left, right, bypass;
 
 void triggerInput() {
-  left  = digitalRead(PIN_SENSOR_LEFT_INPUT);
-  right = digitalRead(PIN_SENSOR_RIGHT_INPUT);
+  bypass  = digitalRead(PIN_SENSOR_BYPASS_INPUT);
+  left    = digitalRead(PIN_SENSOR_LEFT_INPUT);
+  right   = digitalRead(PIN_SENSOR_RIGHT_INPUT);
 
-  if(left != right){
+  if(bypass == LOW && left != right){
     digitalWrite(PIN_SENSOR_LEFT_OUTPUT, !left);
     digitalWrite(PIN_SENSOR_RIGHT_OUTPUT, !right);
   }else{
@@ -37,6 +39,7 @@ void setup() {
   pinMode(PIN_SENSOR_RIGHT_OUTPUT, OUTPUT); digitalWrite(PIN_SENSOR_RIGHT_OUTPUT, HIGH);
   pinMode(PIN_SENSOR_LEFT_INPUT, INPUT);    digitalWrite(PIN_SENSOR_LEFT_INPUT, LOW);
   pinMode(PIN_SENSOR_RIGHT_INPUT, INPUT);   digitalWrite(PIN_SENSOR_RIGHT_INPUT, LOW);
+  pinMode(PIN_SENSOR_BYPASS_INPUT, INPUT);  digitalWrite(PIN_SENSOR_BYPASS_INPUT, LOW);
   
   attachInterrupt(digitalPinToInterrupt(PIN_SENSOR_LEFT_INPUT), triggerInput, CHANGE); 
   attachInterrupt(digitalPinToInterrupt(PIN_SENSOR_RIGHT_INPUT), triggerInput, CHANGE); 
